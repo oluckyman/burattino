8b0a8319be089f295c904d1e4462290457e99f71
ilya <ilya@belsky.in>
2018-07-21 13:06:40 +0200

    Start web-server when connected to wifi

--- main/burattino_main.c
+++ main/burattino_main.c
@@ -10,9 +10,12 @@
 #include "esp_smartconfig.h"
 #include "tcpip_adapter.h"
 #include "nvs_flash.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
 
 /* FreeRTOS event group to signal when we are connected and ready to use wifi */
-static EventGroupHandle_t wifi_event_group;
+static EventGroupHandle_t event_group;
 
 static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
@@ -23,18 +26,18 @@ void smartconfig_task(void * parm);
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 3, NULL);
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        esp_wifi_connect();
-        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    default:
-        break;
+        case SYSTEM_EVENT_STA_START:
+            xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 3, NULL);
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(event_group, CONNECTED_BIT);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            xEventGroupClearBits(event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
     }
     return ESP_OK;
 }
@@ -42,8 +45,6 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 static void initialize_wifi(void)
 {
     tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
@@ -81,7 +82,7 @@ static void sc_callback(smartconfig_status_t status, void *pdata)
                 memcpy(device_ip, (uint8_t* )pdata, 4);
                 ESP_LOGI(TAG, "My IP: %d.%d.%d.%d\n", device_ip[0], device_ip[1], device_ip[2], device_ip[3]);
             }
-            xEventGroupSetBits(wifi_event_group, ESPTOUCH_DONE_BIT);
+            xEventGroupSetBits(event_group, ESPTOUCH_DONE_BIT);
             break;
         default:
             break;
@@ -95,7 +96,7 @@ void smartconfig_task(void * parm)
     ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH) );
     ESP_ERROR_CHECK( esp_smartconfig_start(sc_callback) );
     while (1) {
-        uxBits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY); 
+        uxBits = xEventGroupWaitBits(event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
         if(uxBits & CONNECTED_BIT) {
             ESP_LOGI(TAG, "WiFi Connected to AP");
         }
@@ -107,10 +108,89 @@ void smartconfig_task(void * parm)
     }
 }
 
+static void http_server_netconn_serve(struct netconn *conn)
+{
+    struct netbuf *inbuf;
+    char *buf;
+    u16_t buflen;
+    err_t err;
+
+    /* Read the data from the port, blocking if nothing yet there.
+       We assume the request (the part we care about) is in one netbuf */
+    err = netconn_recv(conn, &inbuf);
+
+    char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+
+    if (err == ERR_OK) {
+        netbuf_data(inbuf, (void**)&buf, &buflen);
+
+        /* Is this an HTTP GET command? (only check the first 5 chars, since
+           there are other formats for GET, and we're keeping it very simple )*/
+        printf("buffer = %s \n", buf);
+        if (buflen>=5 &&
+                buf[0]=='G' &&
+                buf[1]=='E' &&
+                buf[2]=='T' &&
+                buf[3]==' ' &&
+                buf[4]=='/' ) {
+            printf("buf[5] = %c\n", buf[5]);
+            /* Send the HTML header
+             * subtract 1 from the size, since we dont send the \0 in the string
+             * NETCONN_NOCOPY: our data is const static, so no need to copy it
+             */
+
+            netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+
+            char http_index_html[] = "boo";
+            netconn_write(conn, http_index_html, sizeof(http_index_html)-1, NETCONN_NOCOPY);
+        }
+    }
+    /* Close the connection (server closes in HTTP) */
+    netconn_close(conn);
+
+    /* Delete the buffer (netconn_recv gives us ownership,
+       so we have to make sure to deallocate the buffer) */
+    netbuf_delete(inbuf);
+}
+
+static void http_server_task(void *pvParameters)
+{
+    struct netconn *conn, *newconn;
+    err_t err;
+    conn = netconn_new(NETCONN_TCP);
+    netconn_bind(conn, NULL, 80);
+    netconn_listen(conn);
+    ESP_LOGI(TAG, "start webserver");
+    do {
+        err = netconn_accept(conn, &newconn);
+        ESP_LOGI(TAG, "got connection");
+        if (err == ERR_OK) {
+            http_server_netconn_serve(newconn);
+            netconn_delete(newconn);
+        }
+    } while(err == ERR_OK);
+    netconn_close(conn);
+    netconn_delete(conn);
+}
+
+
+static void initialize_webserver()
+{
+    xEventGroupWaitBits(event_group, ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
+
+    xTaskCreate(&http_server_task, "http_server_task", 2048, NULL, 5, NULL);
+}
+
 
 void app_main()
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
-    initialize_wifi();
     ESP_LOGI(TAG, "\n\n\n====== Поехали! ======\n\n");
+    ESP_ERROR_CHECK( nvs_flash_init() );
+
+
+    event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+
+    initialize_wifi();
+    initialize_webserver();
 }
