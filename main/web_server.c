5ca8a7f9de37b1572ce9533a5eac26f5a5b4e7bb
ilya <ilya@belsky.in>
2018-07-23 19:40:42 +0200

    Refactor handle request routine

--- main/web_server.c
+++ main/web_server.c
@@ -9,6 +9,98 @@
 static const char *TAG = "bur[http]";
 
 
+void send_header(struct netconn *conn, int http_code) {
+    char *header;
+    switch (http_code) {
+        case 200:
+            header = "HTTP/1.1 200 OK\r\nContent-type: application/json\r\n\r\n";
+            break;
+        case 400:
+            header = "HTTP/1.1 400 Bad Request\r\nContent-type: application/json\r\n\r\n";
+            break;
+        case 404:
+            header = "HTTP/1.1 404 Not Found\r\n\r\n";
+            break;
+        default:
+            header = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+    }
+    printf(header);
+    netconn_write(conn, header, strlen(header), NETCONN_COPY);
+}
+
+
+void send_body(struct netconn *conn, int http_code, char *message) {
+    char *status = "";
+    switch (http_code) {
+        case 200:
+            status = "OK";
+            break;
+        default:
+            if (strlen(message)) {
+                status = "Error";
+            }
+    }
+
+    if (strlen(status)) {
+        size_t body_max_size = strlen(status) + strlen(message) + 200;
+        char body[body_max_size];
+
+        int cursor = sprintf(body, "{");
+
+        cursor += sprintf(body + cursor, "\"status\":\"%s\"", status);
+
+        if (strlen(message)) {
+            cursor += sprintf(body + cursor, ",");
+            cursor += sprintf(body + cursor, "\"message\":\"%s\"", message);
+        }
+
+        sprintf(body + cursor, "}");
+
+        printf("%s\n", body);
+        netconn_write(conn, body, strlen(body), NETCONN_COPY);
+    }
+}
+
+
+void response(struct netconn *conn, int http_code, char *message) {
+    ESP_LOGI(TAG, "<<<<<<<<<< Response:");
+
+    send_header(conn, http_code);
+    send_body(conn, http_code, message);
+    printf("==========\n");
+}
+
+
+void handle_request(struct netconn *conn, char *request) {
+    char *endpoint = "GET /register";
+    if (strncmp(endpoint, request, strlen(endpoint)) != 0) {
+        response(conn, 404, "");
+        return;
+    }
+
+    int plant_id;
+    int parseResult = sscanf(request + strlen(endpoint), "?plant_id=%d", &plant_id);
+    if (parseResult != 1) {
+        char message[] = "Parameter `plant_id` is required";
+        response(conn, 400, message);
+        return;
+    }
+
+    // TODO: send request to backend here
+
+    // Temporary success response
+    //
+    char *format = "Got plant ID: %d";
+    size_t length = snprintf(NULL, 0, format, plant_id);
+    char message[length];
+    snprintf(message, length + 1, format, plant_id);
+
+    response(conn, 200, message);
+
+    ESP_LOGI(TAG, "OK");
+}
+
+
 static void http_server_netconn_serve(struct netconn *conn) {
     struct netbuf *inbuf;
     char *buf;
@@ -19,38 +111,14 @@ static void http_server_netconn_serve(struct netconn *conn) {
        We assume the request (the part we care about) is in one netbuf */
     err = netconn_recv(conn, &inbuf);
 
-    char http_200_header[] = "HTTP/1.1 200 OK\r\nContent-type: application/json\r\n\r\n";
-    char http_404_header[] = "HTTP/1.1 404 Not Found\r\nContent-type: application/json\r\n\r\n";
-    char http_400_header[] = "HTTP/1.1 400 Bad Request\r\nContent-type: application/json\r\n\r\n";
-
-    ESP_LOGI(TAG, "Got connection");
+    ESP_LOGI(TAG, ">>>>>>>>>> Request:");
     if (err == ERR_OK) {
         netbuf_data(inbuf, (void**)&buf, &buflen);
+        printf("%.*s\n----------\n", buflen, buf);
 
-        printf("> > >\n%.*s\n< < <\n", buflen, buf);
-
-        char *endpoint = "GET /register";
-        if (strncmp(endpoint, buf, strlen(endpoint)) == 0) {
-            // TODO: send request to backend here
-
-            int plantId;
-            if (sscanf(buf + strlen(endpoint), "?plant_id=%d", &plantId) == 1) {
-                printf(" PlantId: %d\n\n", plantId);
-                netconn_write(conn, http_200_header, sizeof(http_200_header)-1, NETCONN_NOCOPY);
-                char http_index_html[] = "{\"status\": \"OK\"}";
-                netconn_write(conn, http_index_html, sizeof(http_index_html)-1, NETCONN_NOCOPY);
-                ESP_LOGI(TAG, "OK");
-            } else {
-                netconn_write(conn, http_400_header, sizeof(http_400_header)-1, NETCONN_NOCOPY);
-                char http_index_html[] = "{\"status\": \"Error\",\"message\":\"`plant_id` is required\"}";
-                netconn_write(conn, http_index_html, sizeof(http_index_html)-1, NETCONN_NOCOPY);
-            }
-
-        } else {
-            netconn_write(conn, http_404_header, sizeof(http_404_header)-1, NETCONN_NOCOPY);
-            ESP_LOGI(TAG, "Not found");
-        }
+        handle_request(conn, buf);
     }
+
     /* Close the connection (server closes in HTTP) */
     netconn_close(conn);
     ESP_LOGI(TAG, "Close connection");
